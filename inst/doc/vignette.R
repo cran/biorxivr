dcd373124afe7b0fca9bd8c78f02659ec4e47114
Edmund Hart <edmund.m.hart@gmail.com>
2015-10-18 00:58:49 +0000

    version 0.1.2

--- inst/doc/vignette.R
+++ inst/doc/vignette.R
@@ -3,6 +3,9 @@ library(biorxivr)
 bxEco <- bx_search("Ecology",limit = 10)
 summary(bxEco)
 
+## ----hidden,echo=F-------------------------------------------------------
+bxEco <- bx_search("Gotelli and Hart",limit = 10)
+
 ## ----get details---------------------------------------------------------
 bxEcoDetails <-  bx_extract(bxEco)
 
